c95ec82b75e1144b2682804394ac981964995767
Baiketli Dijo <baiketlid@gmail.com>
2019-12-03 11:56:23 +0200

    Refactoring tests and using mocks for stubs and fakes

--- src/test/java/com/svs/isbntools/StockManagementTests.java
+++ src/test/java/com/svs/isbntools/StockManagementTests.java
@@ -2,9 +2,8 @@ package com.svs.isbntools;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
